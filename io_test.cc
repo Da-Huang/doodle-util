12069c05426124f5dc961b06157784948d29b143
Da Huang <dhuang.cn@gmail.com>
2016-04-06 19:19:43 +0800

    Applied new logging system to test io module.
    Signed-off-by: Da Huang <dhuang.cn@gmail.com>

--- io_test.cc
+++ io_test.cc
@@ -15,18 +15,18 @@ void TestUsualIO() {
   std::ostringstream oss;
 
   // Writes basic data.
-  int i = 123;
-  Write(&oss, i);
-  float f = 123.45;
-  Write(&oss, f);
-  double d = 123.45;
-  Write(&oss, d);
-  size_t size = 789;
-  Write(&oss, size);
   bool b = true;
   Write(&oss, b);
   char c = ';';
   Write(&oss, c);
+  double d = 123.45;
+  Write(&oss, d);
+  float f = 123.45f;
+  Write(&oss, f);
+  int i = 123;
+  Write(&oss, i);
+  size_t size = 6789;
+  Write(&oss, size);
 
   // Writes string.
   std::string str = "abcd\nefg";
@@ -42,45 +42,37 @@ void TestUsualIO() {
   std::istringstream iss(oss.str());
 
   // Compares between basic data.
-  int i2;
-  Read(&iss, &i2);
-  CHECK_EQ(i, i2) << "i2 mismatched.";
-  float f2;
-  Read(&iss, &f2);
-  CHECK_EQ(f, f2) << "f2 mismatched.";
-  double d2;
-  Read(&iss, &d2);
-  CHECK_EQ(d, d2) << "d2 mismatched.";
-  size_t size2;
-  Read(&iss, &size2);
-  CHECK_EQ(size, size2) << "size2 mismatched.";
   bool b2;
   Read(&iss, &b2);
-  CHECK_EQ(b, b2) << "b2 mismatched.";
+  CHECK_EQ(b, b2);
   char c2;
   Read(&iss, &c2);
-  CHECK_EQ(c, c2) << "c2 mismatched.";
+  CHECK_EQ(c, c2);
+  double d2;
+  Read(&iss, &d2);
+  CHECK_EQ(d, d2);
+  float f2;
+  Read(&iss, &f2);
+  CHECK_EQ(f, f2);
+  int i2;
+  Read(&iss, &i2);
+  CHECK_EQ(i, i2);
+  size_t size2;
+  Read(&iss, &size2);
+  CHECK_EQ(size, size2);
 
   // Compares between string.
   std::string str2;
   Read(&iss, &str2);
-  CHECK_EQ(str, str2) << "str2 mismatched.";
+  CHECK_EQ(str, str2);
 
   // Compares between vector.
   std::vector<int> vi2;
   Read(&iss, &vi2);
-  CHECK_EQ(vi.size(), vi2.size()) << "Size of vi2 mismatched.";
-  for (size_t i = 0; i < vi.size(); ++i) {
-    CHECK_EQ(vi[i], vi2[i]) << "vi2[" << i << "] mismatched.";
-  }
+  CHECK_EQ(vi, vi2);
   std::vector<bool> vb2;
   Read(&iss, &vb2);
-  CHECK_EQ(vb.size(), vb2.size()) << "Size of vb2 mismatched.";
-  for (size_t i = 0; i < vb.size(); ++i) {
-    bool vbi = vb[i];
-    bool vb2i = vb2[i];
-    CHECK_EQ(vbi, vb2i) << "vb2[" << i << "] mismatched.";
-  }
+  CHECK_EQ(vb, vb2);
 }
 
 void TestComplexVectorIO() {
@@ -106,12 +98,13 @@ void TestComplexVectorIO() {
   // Compares between vector.
   std::vector<std::vector<double>> vvi2;
   Read(&iss, &vvi2);
+  CHECK_EQ(vvi, vvi2);
   std::vector<std::vector<std::string>> vvstr2;
   Read(&iss, &vvstr2);
+  CHECK_EQ(vvstr, vvstr2);
   std::vector<std::vector<bool>> vvb2;
   Read(&iss, &vvb2);
-
-  // TODO: Apply new logging system test.
+  CHECK_EQ(vvb, vvb2);
 }
 
 void Test() {
